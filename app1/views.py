8ab5dd57467f590478854641b71d40c531d0c1b5
koonimanjunadha <koonimanjunadha@gmail.com>
2023-03-07 22:14:48 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def manjunath(request):
+    return HttpResponse('<h1><marque>this is my fifth project</h1>/marque')
